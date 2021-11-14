628adf42619d91368a24c49659d339911c61804b
laulin <laulin@nomail.com>
2021-11-14 10:56:45 +0100

    iterate : test with 'q' key

--- Amiga600_keyboard_adapter/src/main.cpp
+++ Amiga600_keyboard_adapter/src/main.cpp
@@ -6,9 +6,12 @@
 #include <util/delay.h>
 #include <avr/io.h>
 #include "amiga_kb.h"
+#include <Keyboard.h>
 
 #define KEYS_SIZE 10
 uint8_t keys[KEYS_SIZE] = {0};
+uint8_t hit = 0;
+uint8_t hold = 0;
 
 void setup()
 {
@@ -17,14 +20,35 @@ void setup()
   init_kb_reader();
   Serial.begin(115200);
   Serial.println("Test keyboard ready !");
+  Keyboard.begin();
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
   uint8_t read_keys = decode_kb(keys, KEYS_SIZE);
-  display_keys(keys, read_keys);
-  delay(10);                     // wait for a second
+  //display_keys(keys, read_keys);
+  delay(10);
+  hit = 0;
+  for (uint8_t i = 0; i < read_keys; i++)
+  {
+    if (keys[i] == 0xBE)
+    {
+      hit = 1;
+    }
+  }
+  if (hit == 1 && hold == 0)
+  {
+    Keyboard.press('a');
+    Serial.println("Pressed");
+    hold = 1;
+  }
+  if(hit == 0 && hold == 1)
+  {
+    Keyboard.release('a');
+    Serial.println("Release");
+    hold = 0;
+  }
   //Serial.println("Line !");
   //Serial.println(read_keys);
 }
