9c286d2e68a5c426cbbd810d7a7edc71edbe57fa
lmoulin <lmoulin@spartan-conseil.fr>
2023-04-30 15:46:54 +0200

    full keymap

--- Linux/serial_keyboard.py
+++ Linux/serial_keyboard.py
@@ -26,18 +26,19 @@ LABELS = {
     0xD2 : "F10",
     0xD1 : "help",
     0xCF : "tild",
-    0xCE : "!",
-    0xCD : "\"",
-    0xCC : "£",
-    0xCA : "%",
-    0xC9 : "^",
-    0xC8 : "&",
-    0xC7 : "*",
-    0xC6 : "(",
-    0xC5 : ")",
-    0xC4 : "_",
-    0xC3 : "+",
-    0xC2 : "|",
+    0xCE : "1",
+    0xCD : "2",
+    0xCC : "3",
+    0xCB : "4",
+    0xCA : "5",
+    0xC9 : "6",
+    0xC8 : "7",
+    0xC7 : "8",
+    0xC6 : "9",
+    0xC5 : "0",
+    0xC4 : "-",
+    0xC3 : "=",
+    0xC2 : "\\",
     0x72  : "backspace",
     0x92 : "del",
     0xBF : "tab",
@@ -51,8 +52,8 @@ LABELS = {
     0xB7 : "i",
     0xB6 : "o",
     0xB5 : "p",
-    0xB4 : "{",
-    0xB3 : "}",
+    0xB4 : "[",
+    0xB3 : "]",
     0xB2 : "enter",
     0x30 : "ctrl",
     0x9F : "caps_locks",
@@ -65,8 +66,8 @@ LABELS = {
     0x98 : "j",
     0x97 : "k",
     0x96 : "l",
-    0x95 : ":",
-    0x94 : "@",
+    0x95 : ";",
+    0x94 : "#",
     0x93 : "void_right",
     0x20 : "shift_left",
     0x7F : "void_left",
@@ -77,28 +78,19 @@ LABELS = {
     0x7A : "b",
     0x79 : "n",
     0x78 : "m",
-    0x77 : "<",
-    0x76 : ">",
-    0x75 : "?",
+    0x77 : ",",
+    0x76 : ".",
+    0x75 : "/",
     0x80 : "shift_right",
-    0xC1 : "arrow_up",
+    0xC1 : "up",
     0x10 : "alt_left",
     0x00 : "amiga_left",
     0x73 : "space",
     0x40 : "amiga_right",
     0x60 : "alt_right",
-    0xB1 : "left_arrow",
-    0x71 : "down_arrow",
-    0x91 : "right_arrow"
-}
-
-modifiers = {
-    "shift" : False,
-    "alt" : False,
-    "amiga_right" : False,
-    "amiga_left" : False,
-    "ctrl" : False,
-    "caps_locks" : False
+    0xB1 : "left",
+    0x71 : "down",
+    0x91 : "right"
 }
 
 SHIFT = 1
@@ -110,6 +102,8 @@ CAPS_LOCKS = 32
 VOID = 0
 IGNORE = -1
 
+modifiers = VOID
+
 modifier_masks = {
     "shift" : SHIFT,
     "alt" : ALT,
@@ -120,45 +114,303 @@ modifier_masks = {
 }
 
 keymap = {
+    "tild":{
+        SHIFT:{"value" : "`"},
+        VOID:{"value" : "~"},
+    },
+    "1":{
+        SHIFT:{"value" : "!"},
+        VOID:{"value" : "1"},
+    },
+    "2":{
+        SHIFT:{"value" : "\""},
+        VOID:{"value" : "2"},
+    },
+    "3":{
+        SHIFT:{"value" : "£"},
+        VOID:{"value" : "3"},
+    },
+    "4":{
+        SHIFT:{"value" : "$"},
+        VOID:{"value" : "4"},
+    },
+    "5":{
+        SHIFT:{"value" : "%"},
+        VOID:{"value" : "5"},
+    },
+    "6":{
+        SHIFT:{"value" : "^"},
+        VOID:{"value" : "6"},
+    },
+    "7":{
+        SHIFT:{"value" : "&"},
+        VOID:{"value" : "7"},
+    },
+    "8":{
+        SHIFT:{"value" : "*"},
+        VOID:{"value" : "8"},
+    },
+    "9":{
+        SHIFT:{"value" : "("},
+        VOID:{"value" : "9"},
+    },
+    "0":{
+        SHIFT:{"value" : ")"},
+        VOID:{"value" : "0"},
+    },
+    "-":{
+        SHIFT:{"value" : "_"},
+        VOID:{"value" : "-"},
+    },
+    "=":{
+        SHIFT:{"value" : "+"},
+        VOID:{"value" : "="},
+    },
+    "\\":{
+        SHIFT:{"value" : "|"},
+        VOID:{"value" : "\\"},
+    },
+
     "q":{
         SHIFT:{"value" : "Q"},
         VOID:{"value" : "q"},
+    },
+    "w":{
+        SHIFT:{"value" : "W"},
+        VOID:{"value" : "w"},
     },
     "e":{
         SHIFT:{"value" : "E"},
         VOID:{"value" : "e"},
         ALT:{"value" : "€"},
     },
+    "r":{
+        SHIFT:{"value" : "R"},
+        VOID:{"value" : "r"},
+    },
+    "t":{
+        SHIFT:{"value" : "T"},
+        VOID:{"value" : "t"},
+    },
+    "y":{
+        SHIFT:{"value" : "Y"},
+        VOID:{"value" : "y"},
+    },
+    "u":{
+        SHIFT:{"value" : "U"},
+        VOID:{"value" : "u"},
+    },
+    "i":{
+        SHIFT:{"value" : "I"},
+        VOID:{"value" : "i"},
+    },
+    "o":{
+        SHIFT:{"value" : "O"},
+        VOID:{"value" : "o"},
+    },
+    "p":{
+        SHIFT:{"value" : "P"},
+        VOID:{"value" : "p"},
+    },
+    "[":{
+        SHIFT:{"value" : "{"},
+        VOID:{"value" : "["},
+    },
+    "]":{
+        SHIFT:{"value" : "}"},
+        VOID:{"value" : "]"},
+    },
+    "a":{
+        SHIFT:{"value" : "A"},
+        VOID:{"value" : "a"},
+    },
+    "s":{
+        SHIFT:{"value" : "S"},
+        VOID:{"value" : "s"},
+    },
+    "d":{
+        SHIFT:{"value" : "D"},
+        VOID:{"value" : "d"},
+    },
+    "f":{
+        SHIFT:{"value" : "F"},
+        VOID:{"value" : "f"},
+    },
+    "g":{
+        SHIFT:{"value" : "G"},
+        VOID:{"value" : "g"},
+    },
+    "h":{
+        SHIFT:{"value" : "H"},
+        VOID:{"value" : "h"},
+    },
+    "j":{
+        SHIFT:{"value" : "J"},
+        VOID:{"value" : "j"},
+    },
+    "k":{
+        SHIFT:{"value" : "K"},
+        VOID:{"value" : "k"},
+    },
+    "l":{
+        SHIFT:{"value" : "L"},
+        VOID:{"value" : "l"},
+    },
+    ";":{
+        SHIFT:{"value" : ":"},
+        VOID:{"value" : ";"},
+    },
+    "#":{
+        SHIFT:{"value" : "@"},
+        VOID:{"value" : "#"},
+    },
+    "z":{
+        SHIFT:{"value" : "Z"},
+        VOID:{"value" : "z"},
+    },
+    "x":{
+        SHIFT:{"value" : "X"},
+        VOID:{"value" : "x"},
+    },
+    "c":{
+        SHIFT:{"value" : "C"},
+        VOID:{"value" : "c"},
+    },
+    "v":{
+        SHIFT:{"value" : "V"},
+        VOID:{"value" : "v"},
+    },
+    "b":{
+        SHIFT:{"value" : "B"},
+        VOID:{"value" : "b"},
+    },
+    "n":{
+        SHIFT:{"value" : "N"},
+        VOID:{"value" : "n"},
+    },
+    "m":{
+        SHIFT:{"value" : "M"},
+        VOID:{"value" : "m"},
+    },
+    ",":{
+        SHIFT:{"value" : "<"},
+        VOID:{"value" : ","},
+    },
+    ".":{
+        SHIFT:{"value" : ">"},
+        VOID:{"value" : "."},
+    },
+    "/":{
+        SHIFT:{"value" : "?"},
+        VOID:{"value" : "/"},
+    },
     "shift_right": {
         IGNORE : {"value" : Key.shift_r}
-    }
+    },
+    "shift_left": {
+        IGNORE : {"value" : Key.shift_l}
+    },
+    "ctrl": {
+        IGNORE : {"value" : Key.ctrl_l}
+    },
+    "alt_right": {
+        IGNORE : {"value" : Key.alt}
+    },
+    "alt_left": {
+        IGNORE : {"value" : Key.alt}
+    },
+    "amiga_left": {
+        IGNORE : {"value" : Key.cmd_l}
+    },
+    "amiga_right": {
+        IGNORE : {"value" : Key.cmd_r}
+    },
+    "space": {
+        VOID : {"value" : " "}
+    },
+    "up": {
+        VOID : {"value" : Key.up}
+    },
+    "down": {
+        VOID : {"value" : Key.down}
+    },
+    "right": {
+        VOID : {"value" : Key.right}
+    },
+    "left": {
+        VOID : {"value" : Key.left}
+    },
+    "enter": {
+        VOID : {"value" : Key.enter}
+    },
+    "backspace": {
+        VOID : {"value" : Key.backspace}
+    },
+    "del": {
+        VOID : {"value" : Key.delete}
+    },
+    "esc": {
+        VOID : {"value" : Key.esc}
+    },
+    "F1": {
+        VOID : {"value" : Key.f1}
+    },
+    "F2": {
+        VOID : {"value" : Key.f2}
+    },
+    "F3": {
+        VOID : {"value" : Key.f3}
+    },
+    "F4": {
+        VOID : {"value" : Key.f4}
+    },
+    "F5": {
+        VOID : {"value" : Key.f5}
+    },
+    "F6": {
+        VOID : {"value" : Key.f6}
+    },
+    "F7": {
+        VOID : {"value" : Key.f7}
+    },
+    "F8": {
+        VOID : {"value" : Key.f8}
+    },
+    "F9": {
+        VOID : {"value" : Key.f9}
+    },
+    "F10": {
+        VOID : {"value" : Key.f10}
+    },
+    "help": {
+        VOID : {"value" : Key.f11}
+    },
+    
+
 }
 
 def set_modifiers():
-    modifiers["shift"] = False
-    modifiers["alt"] = False
-    modifiers["ctrl"] = False
-    modifiers["amiga_right"] = False
-    modifiers["amiga_left"] = False
-
-    if "shift_right" in pressed_keys or "shift_left" in pressed_keys:
-        modifiers["shift"] = True
+    global modifiers
+    modifiers = modifiers & CAPS_LOCKS
 
     if "alt_right" in pressed_keys or "alt_left" in pressed_keys:
-        modifiers["alt"] = True
+        modifiers |= ALT
 
     if "ctrl" in pressed_keys:
-        modifiers["ctrl"] = True
+        modifiers |= CTRL
 
     if "amiga_right" in pressed_keys:
-        modifiers["amiga_right"] = True
+        modifiers |= AMIGA_RIGHT
 
     if "amiga_left" in pressed_keys:
-        modifiers["amiga_left"] = True
+        modifiers |= AMIGA_LEFT
 
     if "caps_locks" in pressed_keys:
         if pressed_keys["caps_locks"] == 0:
-            modifiers["caps_locks"] = True ^ modifiers["caps_locks"]
+            modifiers ^= CAPS_LOCKS
+
+    if ("shift_right" in pressed_keys or "shift_left" in pressed_keys) ^ (modifiers & CAPS_LOCKS == CAPS_LOCKS):
+        modifiers |= SHIFT
 
 
 def read_kbn_frame(ser:serial.Serial)->bytes:
@@ -199,13 +451,6 @@ def read_kbn_frame(ser:serial.Serial)->bytes:
 
 applied_keymap = {}
 
-def make_mask():
-    mask = VOID
-    for modifier, value in modifiers.items():
-        if value:
-            mask |= modifier_masks[modifier]
-    return mask
-
 def get_key_configuration(labeled_key, mask):
     return keymap.get(labeled_key, {}).get(mask, {})
 
@@ -216,7 +461,7 @@ def apply_keymap():
         if "value" in get_key_configuration(labeled_key, IGNORE):
             conf = get_key_configuration(labeled_key, IGNORE)
         else:
-            mask = make_mask()
+            mask = modifiers & (~CAPS_LOCKS)
             conf = get_key_configuration(labeled_key, mask)
 
         if "value" in conf:
